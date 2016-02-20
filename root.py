fa9ca034280cc716eea9f3933a6c1673d9c77fbe
abhinavvarma <abhinav.varma123@gmail.com>
2016-02-21 01:17:17 +0530

    watch specific ref

--- root.py
+++ root.py
@@ -1,21 +1,23 @@
-from flask import Flask
-from flask import request
+import json
 from subprocess import call
 
-from settings import DEPLOYMENT_SCRIPT_PATH, DEPLOYMENT_LOG_PATH
+from flask import Flask
+from flask import request
+from settings import DEPLOYMENT_SCRIPT_PATH, DEPLOYMENT_LOG_PATH, REF_TO_WATCH
 
 app = Flask(__name__)
 
 
 @app.route("/deploy/", methods=["POST"])
 def deploy():
+    push_data = json.loads(request.data)
+    if push_data.ge('ref') != REF_TO_WATCH:
+        return "Sorry! I am not watching this ref"
     errors = ""
     try:
-        with open(DEPLOYMENT_LOG_PATH, 'w') as logfile:
-            logfile.write(str(request.data))
         call([DEPLOYMENT_SCRIPT_PATH])
     except Exception as e:
-        errors = "Error Occured: %s\n"%e
+        errors = "Error Occurred: %s\n" % e
     try:
         with open(DEPLOYMENT_LOG_PATH, 'r') as logfile:
             log = logfile.read().replace('\n', '<br>')
@@ -23,7 +25,6 @@ def deploy():
         return "Job started.\n Could not display the log file.\n" + str(fe)
     return errors + log
 
+
 if __name__ == "__main__":
     app.run()
-
-"testing hook"
--- settings.py
+++ settings.py
@@ -1,3 +1,7 @@
 DEPLOYMENT_SCRIPT_PATH = "/home/ubuntu/deployments/scripts/build_and_email.sh"
 
 DEPLOYMENT_LOG_PATH = "/home/ubuntu/logs/deployment.log"
+
+BRANCH_TO_WATCH = "stable"
+
+REF_TO_WATCH = "refs/heads/" + BRANCH_TO_WATCH
