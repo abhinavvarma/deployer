78d92f564afc95c0f53b98ebbfe645f06334cb1b
abhinavvarma <abhinav.varma123@gmail.com>
2016-02-20 22:44:03 +0530

    returning log file

--- root.py
+++ root.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from subprocess import call
 
-from settings import DEPLOYMENT_SCRIPT_PATH
+from settings import DEPLOYMENT_SCRIPT_PATH, DEPLOYMENT_LOG_PATH
 
 app = Flask(__name__)
 
@@ -11,8 +11,13 @@ def deploy():
     try:
         call([DEPLOYMENT_SCRIPT_PATH])
     except Exception as e:
-        return str(e)
-    return "Job started"
+        return "Error Occured: " + str(e)
+    try:
+        with open(DEPLOYMENT_LOG_PATH, 'r') as logfile:
+            log = logfile.read().replace('\n', '<br>')
+    except Exception as fe:
+        return "Job started.\n Could not display the log file.\n" + str(fe)
+    return log
 
 if __name__ == "__main__":
     app.run()
--- settings.py
+++ settings.py
@@ -1,1 +1,3 @@
 DEPLOYMENT_SCRIPT_PATH = "/home/ubuntu/deployments/scripts/deploy_root_robot.sh"
+
+DEPLOYMENT_LOG_PATH = "/home/ubuntu/logs/deployment.log"
