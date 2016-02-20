4a90528f55ac41ea685c32bf2cb8a4061117d17f
abhinavvarma <abhinav.varma123@gmail.com>
2016-02-20 22:49:04 +0530

    reloading apache, not restart

--- root.py
+++ root.py
@@ -8,16 +8,17 @@ app = Flask(__name__)
 
 @app.route("/deploy/")
 def deploy():
+    errors = ""
     try:
         call([DEPLOYMENT_SCRIPT_PATH])
     except Exception as e:
-        return "Error Occured: " + str(e)
+        errors = "Error Occured: %s\n"%e
     try:
         with open(DEPLOYMENT_LOG_PATH, 'r') as logfile:
             log = logfile.read().replace('\n', '<br>')
     except Exception as fe:
         return "Job started.\n Could not display the log file.\n" + str(fe)
-    return log
+    return errors + log
 
 if __name__ == "__main__":
     app.run()
