8c6cf52610188b9fa724686f8e69fff3e00413b7
abhinavvarma <abhinav.varma123@gmail.com>
2016-02-21 01:22:18 +0530

    rename

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-DEPLOYMENT_SCRIPT_PATH = "/home/ubuntu/deployments/scripts/build_and_email.sh"
+DEPLOYMENT_SCRIPT_PATH = "/home/ubuntu/deployments/scripts/build_and_log.sh"
 
 DEPLOYMENT_LOG_PATH = "/home/ubuntu/logs/deployment.log"
 
