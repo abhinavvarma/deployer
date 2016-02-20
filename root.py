2c57ba5e853bef9da68bb5f27ece9104c69f3e06
abhinavvarma <abhinav.varma123@gmail.com>
2016-02-21 00:56:27 +0530

    method type post

--- root.py
+++ root.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 from subprocess import call
 
 from settings import DEPLOYMENT_SCRIPT_PATH, DEPLOYMENT_LOG_PATH
@@ -6,7 +7,7 @@ from settings import DEPLOYMENT_SCRIPT_PATH, DEPLOYMENT_LOG_PATH
 app = Flask(__name__)
 
 
-@app.route("/deploy/")
+@app.route("/deploy/", methods=["POST"])
 def deploy():
     errors = ""
     try:
@@ -18,7 +19,7 @@ def deploy():
             log = logfile.read().replace('\n', '<br>')
     except Exception as fe:
         return "Job started.\n Could not display the log file.\n" + str(fe)
-    return errors + log
+    return str(request.data)+errors + log
 
 if __name__ == "__main__":
     app.run()
