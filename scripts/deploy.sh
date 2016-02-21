e3e123dc0838e9935bf04a42c8acb4dc88ccdc61
abhinavvarma <abhinav.varma123@gmail.com>
2016-02-21 23:12:35 +0530

    -

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,6 @@
 
 source /home/ubuntu/deployments/deployer/scripts/config.sh
 
-"$DEPLOYMENT_ROOT/deployer/scripts/build.sh >> $DEPLOYMENT_LOG_PATH 2>&1" || { echo "Error: Build failed"; exit 1;}
+"$DEPLOYMENT_ROOT/deployer/scripts/build.sh" >> $DEPLOYMENT_LOG_PATH 2>&1 || { echo "Error: Build failed"; exit 1;}
 "cd $DEPLOYMENT_PATH" || { echo "Error: Could not cd to $DEPLOYMENT_PATH"; exit 1;}
-"./bin/production/deploy.sh >> $DEPLOYMENT_LOG_PATH 2>&1" || { echo "Error: Deploy failed"; exit 1;}
+"./bin/production/deploy.sh" >> $DEPLOYMENT_LOG_PATH 2>&1 || { echo "Error: Deploy failed"; exit 1;}
