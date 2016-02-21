c3a991ae6f648d7872dd6c287d22b7b975f2d80e
abhinavvarma <abhinav.varma123@gmail.com>
2016-02-22 01:14:22 +0530

    mail to godavari

--- scripts/deploy_and_mail.sh
+++ scripts/deploy_and_mail.sh
@@ -5,15 +5,13 @@ source /home/ubuntu/deployments/deployer/scripts/config.sh
 ist=$(TZ=IST-5:30 date)
 SUCCESS_MAIL_SUBJECT="Auto-Deployment ($ist): Godavari is successfully redeployed"
 FAILURE_MAIL_SUBJECT="Auto-Deployment ($ist): Godavari deployment failed"
-MAIL_RECEPIENT="abhinav.varma123@gmail.com"
+MAIL_RECEPIENT="godavari@customfurnish.com"
 
 "$DEPLOYMENT_ROOT/deployer/scripts/deploy.sh" > $DEPLOYMENT_LOG_PATH 2>&1 || {
     echo "Error: Deploy failed";
-#    mail -s "$FAILURE_MAIL_SUBJECT" -aFrom:autodep.godavari $MAIL_RECEPIENT < $DEPLOYMENT_LOG_PATH
+    mail -s "$FAILURE_MAIL_SUBJECT" -aFrom:autodep.godavari $MAIL_RECEPIENT < $DEPLOYMENT_LOG_PATH
     exit 1;
 }
 
 mail -s "$SUCCESS_MAIL_SUBJECT" -aFrom:autodep.godavari $MAIL_RECEPIENT < $DEPLOYMENT_LOG_PATH
 exit 0;
-
-#/home/ubuntu/logs/deployment-watcher.log IN_CLOSE_WRITE /home/ubuntu/deployments/scripts/deploy_root.sh
