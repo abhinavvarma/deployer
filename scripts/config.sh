ad5d3081f5e78ffe2c0d14f806a8bb111e6335a0
abhinavvarma <abhinav.varma123@gmail.com>
2016-02-21 23:02:07 +0530

    -

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-DEPLOYMENT_ROOT="/home/ubuntu/deployments"
+DEPLOYMENT_ROOT=/home/ubuntu/deployments
 DEPLOYMENT_FOLDER="godavari"
-DEPLOYMENT_PATH="$DEPLOYMENT_ROOT/$DEPLOYMENT_FOLDER"
-DEPLOYMENT_LOG_PATH="/home/ubuntu/logs/deployment.log"
+DEPLOYMENT_PATH=$DEPLOYMENT_ROOT"/"$DEPLOYMENT_FOLDER
+DEPLOYMENT_LOG_PATH=/home/ubuntu/logs/deployment.log
 
 DEPLOYMENT_BRANCH="deployment@abhinav"
