cb88147f337c216d41812bd616ca3905b29855b3
abhinavvarma <abhinav.varma123@gmail.com>
2016-02-22 01:24:11 +0530

    mail to godavari

--- scripts/deploy_and_mail.sh
+++ scripts/deploy_and_mail.sh
@@ -5,6 +5,7 @@ source /home/ubuntu/deployments/deployer/scripts/config.sh
 ist=$(TZ=IST-5:30 date)
 SUCCESS_MAIL_SUBJECT="Auto-Deployment ($ist): Godavari is successfully redeployed"
 FAILURE_MAIL_SUBJECT="Auto-Deployment ($ist): Godavari deployment failed"
+
 MAIL_RECEPIENT="godavari@customfurnish.com"
 
 "$DEPLOYMENT_ROOT/deployer/scripts/deploy.sh" > $DEPLOYMENT_LOG_PATH 2>&1 || {
