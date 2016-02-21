55d9c351f615e7a39d593873665c3ea2962f8449
abhinavvarma <abhinav.varma123@gmail.com>
2016-02-22 00:03:53 +0530

    better messages

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,16 +6,18 @@ source /home/ubuntu/deployments/deployer/scripts/config.sh
 
 cd ${DEPLOYMENT_PATH} || { echo "Error: Could not cd to $DEPLOYMENT_PATH"; exit 1;}
 if [ "$PWD" == "$DEPLOYMENT_PATH" ]; then
-  echo "Moved to $PWD"
+  echo "Location: $PWD"
 else
-  echo "Could not move to $DEPLOYMENT_PATH"
+  echo "Could not cd to $DEPLOYMENT_PATH"
   exit 1;
 fi
 git reset --hard || { echo "Error: Could not git reset"; exit 1;}
 git checkout $DEPLOYMENT_BRANCH || { echo "Error: Could not checkout to $DEPLOYMENT_BRANCH"; exit 1;}
 git pull || { echo "Error: Could not pull changes"; exit 1;}
 echo "Pull successful"
+echo "Following is the commit that is being deployed:"
 git log -1
 cp $DEPLOYMENT_ROOT/properties.override.json $DEPLOYMENT_PATH/properties/properties.override.json || { echo "Error: Could not copy the properties.override.json"; exit 1;}
+echo "Copied the properties.override.json"
 "bin/production/setup.sh" || { echo "Error: setup.sh failed"; exit 1; }
 exit 0;
