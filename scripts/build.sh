10c43fd780c73d13431c04afa67475b0f43f43dd
abhinavvarma <abhinav.varma123@gmail.com>
2016-02-21 22:45:06 +0530

    pull successful msg

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,6 +8,7 @@ source /home/ubuntu/deployments/deployer/scripts/config.sh
 "git reset --hard" || { echo "Error: Could not git reset"; exit 1;}
 "git checkout $DEPLOYMENT_BRANCH" || { echo "Error: Could not checkout to $DEPLOYMENT_BRANCH"; exit 1;}
 "git pull" || { echo "Error: Could not pull changes"; exit 1;}
+echo "Pull successful"
 "cp $DEPLOYMENT_ROOT/properties.override.json $DEPLOYMENT_PATH/properties/properties.override.json" || { echo "Error: Could not copy the properties.override.json"; exit 1;}
 "bin/production/setup.sh" || { echo "Error: setup.sh failed"; exit 1; }
 exit 0;
