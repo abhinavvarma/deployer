79f5e206eb0685a63fba48ced95d732feff447cf
Abhinav <abhinav.varma123@gmail.com>
2016-04-29 14:08:24 +0530

    changed monitor branch

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ DEPLOYMENT_WATCHED_LOG_PATH = "/home/ubuntu/logs/deployment-watcher.log"
 
 DEPLOYMENT_LOG_PATH = "/home/ubuntu/logs/deployment.log"
 
-BRANCH_TO_WATCH = "stable"
+BRANCH_TO_WATCH = "release-1"
 
 REF_TO_WATCH = "refs/heads/" + BRANCH_TO_WATCH
 
