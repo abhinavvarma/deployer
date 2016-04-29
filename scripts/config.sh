44cbb524db4f384645c44d186854ec5790e5ccb2
abhinav <abhinav.varma123@gmail.com>
2016-04-29 14:23:57 +0530

    DEPLOYMENT_BRANCH getting from settings.py

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+DIR=$( dirname "${BASH_SOURCE[0]}" )
+ROOT="$DIR/.."
+
+ALL_RESULTS=$(python -c "import imp;settings = imp.load_source('settings', '$ROOT/settings.py');print settings.BRANCH_TO_WATCH")
+
 DEPLOYMENT_ROOT="/home/ubuntu/deployments"
 DEPLOYMENT_FOLDER="godavari"
 DEPLOYMENT_PATH=${DEPLOYMENT_ROOT}"/"${DEPLOYMENT_FOLDER}
 DEPLOYMENT_LOG_PATH="/home/ubuntu/logs/deployment.log"
-DEPLOYMENT_BRANCH="stable"
+DEPLOYMENT_BRANCH=$(echo $ALL_RESULTS | cut -d' ' -f1)
+
+echo $DEPLOYMENT_BRANCH
